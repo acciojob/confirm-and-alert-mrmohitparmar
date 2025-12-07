b4fca1d284656a028b90411fac5d090eb84e248f
Mohit Parmar <127684291+mrmohitparmar@users.noreply.github.com>
2025-12-07 16:44:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let rs = confirm("Do you want to proceed?")
+if(rs){
+	alert("You clicked OK. Proceeding...")
+}
+else{
+	alert("You clicked Cancel. Exiting...")
+	
+}
